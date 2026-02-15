c8cd8397576ead9bedb0adf4763f25f0bfb8c4d2
ItzDuolingo <jantemin437@gmail.com>
2026-02-15 01:55:35 +0100

    Enhance power options to set username before actions
    Add username state setting before rebooting or shutting down.

--- Operating_System/lib/power.lua
+++ Operating_System/lib/power.lua
@@ -1,11 +1,19 @@
+local state = require("lib.state")
+
+
 local M = {}
 -- ====================================================================================================
 -- this returns the functions to shutdown and reboot which is later used in other menus to also be able
 -- to select to reboot or shutdown allowing to exit the system or restart it if something goes wrong
 -- ====================================================================================================
 M.powerOptionsActions = {
-    {name = "Restart",  action = function() os.reboot() end},
-    {name = "Shutdown", action = function() os.shutdown() end},
+    {name = "Restart",  action = function()
+        state.setUsername()
+        os.reboot() end},
+
+    {name = "Shutdown", action = function()
+        state.setUsername()
+        os.shutdown() end},
 }
 
 return M
