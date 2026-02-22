74c1e93f7482f7fed0839d51dba4dcd3fe446ec8
ItzDuolingo <jantemin437@gmail.com>
2026-02-22 23:30:00 +0100

    Add settings management and theme options

--- Operating_System/lib/settingsManager.lua
+++ Operating_System/lib/settingsManager.lua
@@ -7,22 +7,55 @@ local username = state.getUsername()
 
 local M = {}
 
+M.current = {
+    background = colors.lightGray,
+    text = colors.black,
+    clockEnabled = true,
+    timeFormat = "DD/MM/YYYY",
+}
+
+M.clockState = {
+     {name = "Toggle"}, 
+}
+
+M.formatOpt = {
+    {name = "DD/MM/YYYY"},
+    {name = "MM/DD/YYYY"},
+    {name = "YYYY/MM/DD"},
+}
+
 function M.loadSettings(username)
+    local settings 
+
     if username == nil then 
-        return defaultSettings 
-    end
-    local path = "/operatingSystem/users/"..username.."/settings.json"
-    if not fs.exists(path) then 
-        local file = fs.open(path, "w")
-        file.write(textutils.serialize(defaultSettings))
+        settings = defaultSettings  
+    else
+
+        local path = "/operatingSystem/users/"..username.."/settings.json"
+        if not fs.exists(path) then 
+            local file = fs.open(path, "w")
+            file.write(textutils.serialize(defaultSettings))
+            file.close()
+        end
+
+        local file = fs.open(path, "r")
+        settings = textutils.unserialize(file.readAll())
         file.close()
     end
 
-    local file = fs.open(path, "r")
-    local settings = textutils.unserialize(file.readAll())
-    file.close()
+    if settings and settings.ui and username then
+        M.current.background = colors[settings.ui.background] or colors.lightGray
+        M.current.text = colors[settings.ui.textColor.ui] or colors.black
+        M.current.timeFormat = settings.date.format 
+        M.current.clockEnabled = settings.clock.enabled  
+        if M.current.clockEnabled == nil then 
+            M.current.clockEnabled = true 
+        end
+    end
+
     return settings
 end
+
 
 function M.restoreSettings(username)
     local path = "/operatingSystem/users/"..username.."/settings.json"
@@ -32,6 +65,40 @@ function M.restoreSettings(username)
     file.write(textutils.serialize(defaultSettings))
     file.close()
 end
+
+function M.apply(themeData)
+    M.current.background = colors[themeData.background] or colors.lightGray
+    M.current.text = colors[themeData.text] or colors.black
+end
+
+function M.toggleClock(username)
+    local settings = M.loadSettings(username)
+    local username = state.getUsername()
+
+    settings.clock.enabled = not settings.clock.enabled
+
+    local path = "/operatingSystem/users/"..username.."/settings.json"
+    local file = fs.open(path, "w")
+    file.write(textutils.serialize(settings))
+    file.close()
+
+    M.current.clockEnabled = settings.clock.enabled
+    return M.current.clockEnabled
+end
+
+function M.applyTimeFormat(username, format)
+    local settings = M.loadSettings(username)
+    local username = state.getUsername()
+
+    settings.date.format = format.name
+    M.current.timeFormat = format.name
+
+    local path = "/operatingSystem/users/"..username.."/settings.json"
+    local file = fs.open(path, "w")
+    file.write(textutils.serialize(settings))
+    file.close()
+end
+
 -- options to be used for changing theme
 M.themes = {
     {name = "Dark", data = {background = "black", text = "white"}},
