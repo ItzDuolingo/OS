772d13d446122de704921cfe9cf2f7ead125eb07
ItzDuolingo <jantemin437@gmail.com>
2026-03-08 21:35:40 +0100

    Refactor app loading and permissions in desktop.lua
    Refactored app loading logic to read from user-specific apps.json file and updated permission checks.

--- Operating_System/desktop.lua
+++ Operating_System/desktop.lua
@@ -1,8 +1,8 @@
+package.path = "/operatingSystemCode/?.lua;/operatingSystemCode/?/init.lua;" .. package.path
 -- required modules
 local users = require("lib.users")
 local state = require("lib.state")
-local appsLib = require("lib.appsData")
-local apps = appsLib.defaultApps()
+local appsLib = require("lib.defaultApps")
 local header = require("UI.header")
 local navigation = require("UI.navigationHelp")
 local selectionLib = require("lib.selection")
@@ -10,31 +10,40 @@ local state = require("lib.state")
 local powerLib = require("lib.power")
 local powerOptionsActions = powerLib.powerOptionsActions
 
--- Guest function removed for now
 local username = state.getUsername() 
-
-local optionsActions = {}
 local meta = users.loadUserMeta(username)
+while true do 
+    local optionsActions = {}
+    local appsPath = "/operatingSystem/users/"..username.."/apps.json"
 
-for appName, app in pairs(apps) do 
-    if app.installed then 
-        local allowed = true 
-            
-        if app.requires == "admin" and not meta.admin then 
-            allowed = false
-        elseif app.requires == "dev" and not meta.devMode then 
-            allowed = false
-        end   
-            
-        if allowed then 
-            table.insert(optionsActions, {
-                name = appName:gsub("_", " "),
-                action = function()
-                    shell.run(app.code_path)
-                end
-            })
+    local file = fs.open(appsPath, "r")
+    local apps = textutils.unserialize(file.readAll())
+    file.close()
+
+    -- getting app names, checking if installed or not, permission check and insertion into table 
+    for appName, app in pairs(apps) do 
+        if app.installed then 
+            local allowed = true 
+                    
+            if app.requires == "admin" and not meta.admin then 
+                allowed = false
+            elseif app.requires == "dev" and not meta.devMode then 
+                allowed = false
+            end   
+                    
+            if allowed then 
+                table.insert(optionsActions, {
+                    name = appName:gsub("_", " "),
+                    action = function()
+                        shell.run(app.code_path)
+                        return true 
+                    end
+                })
+            end
         end
+    
     end
+
+    
+    selectionLib.selection(optionsActions, 1, 5, 42, 18, "", "=== desktop ===", 20, 3, false)
 end
- 
-selectionLib.selection(powerOptionsActions, optionsActions, 1, 5, 42, 18, "", username, "=== desktop ===", 20, 3, false)  
