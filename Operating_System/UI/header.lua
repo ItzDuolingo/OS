262d6ba36743cd3eeacda8d8d574fff760b68b78
ItzDuolingo <jantemin437@gmail.com>
2026-03-19 20:44:17 +0100

    Update header.lua

--- Operating_System/UI/header.lua
+++ Operating_System/UI/header.lua
@@ -6,46 +6,48 @@ local state = require("lib.state")
 
 local M = {}
     
--- ========================================================================================================
--- this function gets the local time of your computer and writes it on the right upper side of the terminal,
+-- ========================================================================================
+-- this function gets the ingame time and writes it on the right upper side of the terminal
 -- whenever a minute goes by the clock clears itself and writes it again
--- ========================================================================================================
+-- ========================================================================================
 function M.drawClock()
     local username = state.getUsername()
     local getSettings = settings.loadSettings(username)
-    local clock = getSettings.clock.enabled
-    if clock == true then 
-        local time = os.time("local")
-        local clock = textutils.formatTime(time, false)
-        term.setCursorPos(44,1)
-        write("        ")
-        term.setCursorPos(44,1)
+    local clockState = getSettings.clock.enabled
+    local width, height = term.getSize()
+    local time = os.time("local")
+    local clock = textutils.formatTime(time, false)
+    if clockState == true then 
+        local x = math.floor((width - #clock)) + 1
+        term.setCursorPos(x,1)
+        string.rep(" ", #clock)
+        term.setCursorPos(x,1)
         write(clock)
     end
 end
--- ========================================================================================================
--- takes the username variable that it gets and writes "logged in as..." on the right upper side and also
--- gets local date of your computer and draws the date below the clock, if no username variable is provided,
--- it won't draw the "logged in as..." text due to usage in menus that dont need it
--- ========================================================================================================
+-- ==================================================
+-- Gets ingame date, username, writes header and date
+-- ==================================================
 function M.drawHeader(username)
+    local width, height = term.getSize()
     local getSettings = settings.loadSettings(username)
     local clock = getSettings.clock.enabled
     local timeFormat = getSettings.date.format
-    local x, y = 42, 2
-    if clock == false then 
-        x, y = 42, 1
-    end
-
     local date 
 
     if timeFormat == "DD/MM/YYYY" then
-            date = os.date("%d.%m.%Y") 
-        elseif timeFormat == "MM/DD/YYYY" then 
-            date = os.date("%m.%d.%Y")
-        elseif timeFormat == "YYYY/MM/DD" then
-            date = os.date("%Y.%m.%d") 
-        end
+        date = os.date("%d.%m.%Y") 
+    elseif timeFormat == "MM/DD/YYYY" then 
+        date = os.date("%m.%d.%Y")
+    elseif timeFormat == "YYYY/MM/DD" then
+        date = os.date("%Y.%m.%d") 
+    end
+
+    local x = math.floor((width - #date)) + 1
+    local y = 2
+    if clock == false then 
+        y = 1
+    end
 
     if username then 
         term.setCursorPos(1,1)
